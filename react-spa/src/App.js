434cc19b271907906264ab79ea145719df91ba71
romerodevelopments <gandresrp@gmail.com>
2021-12-24 19:11:47 -0300

    routing en el cliente - definir rutas

--- react-spa/src/App.js
+++ react-spa/src/App.js
@@ -1,7 +1,15 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-let Hello = () => {
-  return <h1>Hola mundo</h1>
+let NotImplemented = () => {
+  return <h1>Esta página aún no está disponible</h1>
+}
+
+let HolaIndex = () => {
+  return <h1>Hola index</h1>
+}
+
+let HolaVideos = () => {
+  return <h1>Hola vídeos</h1>
 }
 
 function App() {
@@ -9,7 +17,21 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Hello/>}/>
+          <Route path="/" element={<HolaIndex/>}/>
+          <Route path="/videos" element={<HolaVideos/>}/>
+        </Routes>
+
+        <Routes>
+          <Route path="/" element={<NotImplemented/>}/>
+          <Route path="/usuarios/registro" element={<NotImplemented/>}/>
+          <Route path="/usuarios/login" element={<NotImplemented/>}/>
+
+          <Route path="/usuarios/:id" element={<NotImplemented/>}/>
+          <Route path="/usuarios/:id/videos" element={<NotImplemented/>}/>
+
+          <Route path="/videos" element={<NotImplemented/>}/>
+          <Route path="/videos/nuevo" element={<NotImplemented/>}/>
+          <Route path="/videos/:id" element={<NotImplemented/>}/>
         </Routes>
       </BrowserRouter>
     </div>
