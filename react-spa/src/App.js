e2453f09cdfad7a0c8307e42c363a46e4f5e7c16
romerodevelopments <gandresrp@gmail.com>
2021-12-26 17:21:16 -0300

    autenticacion de usuarios - instalar y configurar redux

--- react-spa/src/App.js
+++ react-spa/src/App.js
@@ -1,6 +1,6 @@
 import {
-  BrowserRouter, 
-  Routes, 
+  BrowserRouter,
+  Routes,
   Route,
   Link,
   Outlet,
@@ -10,11 +10,17 @@ import {
   useLocation
 } from 'react-router-dom';
 
+import {
+  Provider
+} from 'react-redux';
+
+import { store } from './store';
+
 let NotImplemented = () => {
   return (
     <>
       <Link to="/videos">Ir a videos</Link>
-      <br/>
+      <br />
       <a href='/videos'>Ir a video tradicional</a>
       <h1>Esta página aún no está disponible</h1>
     </>
@@ -25,7 +31,7 @@ let Error404 = () => {
   return (
     <>
       <Link to="/">Regresar al inicio</Link>
-      <br/>
+      <br />
       <h1>Recurso no encontrado - 404</h1>
     </>
   )
@@ -50,14 +56,14 @@ let UsuariosOutlet = () => {
     <>
       <button onClick={redirect}>Ir al home</button>
       <p>Hola desde usuarios</p>
-      <Outlet/>
+      <Outlet />
     </>
   )
 }
 
 let VideoShow = () => {
   //let params = useParams();  hook que recibe los params en el path del routing
-  let {id, wildcard} = useParams(); //destructuring, extrae el valor del objeto json
+  let { id, wildcard } = useParams(); //destructuring, extrae el valor del objeto json
   console.log(id, wildcard);
 
   let location = useLocation();
@@ -68,14 +74,14 @@ let VideoShow = () => {
   console.log(queryParams);
 
   //de esta forma pordemos obtener los params recibidos como objetos tipo hash con clave y valor
-  for(let p of queryParams) {
+  for (let p of queryParams) {
     //console.log(p)
     console.log(p[0]);
     console.log(p[1]);
-  }; 
+  };
 
 
-  return(
+  return (
     <p>{id}</p>
   )
 }
@@ -84,25 +90,27 @@ function App() {
   const isAuth = false;
   return (
     <div className="App">
-      <BrowserRouter>          
-        <Routes>
-          <Route path="/" element={<NotImplemented/>}/>
-          <Route path="*" element={<Error404/>}/>
+      <BrowserRouter>
+        <Provider store={store}>
+          <Routes>
+            <Route path="/" element={<NotImplemented />} />
+            <Route path="*" element={<Error404 />} />
 
-          <Route path="/usuarios" element={isAuth ? <Navigate to="/" /> : <UsuariosOutlet/>}>
-            <Route path="registro" element={<NotImplemented/>}/>
-            <Route path="login" element={<NotImplemented/>}/>
-            <Route path=":id" element={<NotImplemented/>}/>
-            <Route path=":id/videos" element={<NotImplemented/>}/>
-          </Route>
+            <Route path="/usuarios" element={isAuth ? <Navigate to="/" /> : <UsuariosOutlet />}>
+              <Route path="registro" element={<NotImplemented />} />
+              <Route path="login" element={<NotImplemented />} />
+              <Route path=":id" element={<NotImplemented />} />
+              <Route path=":id/videos" element={<NotImplemented />} />
+            </Route>
 
-          <Route path="/videos">
-            <Route path="" element={<NotImplemented/>}/>
-            <Route path="nuevo" element={<NotImplemented/>}/>
-            <Route path=":id" element={<NotImplemented/>}/>
-            <Route path=":id/:wildcard" element={<VideoShow/>}/>
-          </Route>
-        </Routes>
+            <Route path="/videos">
+              <Route path="" element={<NotImplemented />} />
+              <Route path="nuevo" element={<NotImplemented />} />
+              <Route path=":id" element={<NotImplemented />} />
+              <Route path=":id/:wildcard" element={<VideoShow />} />
+            </Route>
+          </Routes>
+        </Provider>
       </BrowserRouter>
     </div>
   );
